fa853c6c3175493ed40fb99e77012e45444f6dc7
Benjamin Weber <one@bwe.im>
2025-05-20 10:29:28 +0200

    fixing haskell benchmarking

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
   - peak memory allocation: 3 MB, runtime: 70 ms
   - run: `cargo run`
 - Haskell, scalpel:
-  - peak memory allocation: 261 MB, runtime: 130 ms
+  - peak memory allocation: 175 MB, runtime: 160 ms
   - run: `cabal run`
--- haskell-scraper/app/Main.hs
+++ haskell-scraper/app/Main.hs
@@ -9,14 +9,16 @@ import Data.Maybe (fromMaybe)
 import Criterion.Main
 import Criterion.Measurement
 import GHC.Stats as GHC
+import System.Mem
 
 scraper :: IO ()
 scraper = do
   html <- B.readFile "../amazon.com.html"
+  _ <- performMajorGC
   let res = scrapeStringLike html $ texts "h2"
+  pPrint res
   gcs <- getGCStatistics
   let pma = maybe "" (show . gcStatsPeakMegabytesAllocated) gcs
-  pPrint res
   putStrLn $ "peak memory allocated in MB: " ++ pma
 
 main :: IO ()
